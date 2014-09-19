e15bd1817f6f2cd741e2f869e7ac6159f5dc6fe6
Nguyen Ta Duy <nguyentaduy@hotmail.com>
2014-09-19 12:37:27 +0800

    add test

--- src/TextBuddy/TextBuddyTest.java
+++ src/TextBuddy/TextBuddyTest.java
@@ -6,15 +6,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TextBuddyTest {
-	
-	
+
+	/**
+	 * Initialize a new file empty named text.txt when start each test
+	 */
 	@Before
 	public void initialize() {
 		String a = "text.txt";
 		TextBuddy.initialize(a);
 		TextBuddy.executeCommand("clear");
 	}
-	
+
+	/**
+	 * These following two test cases are normal cases for sort
+	 */
+
 	@Test
 	public void testSort_1() {
 
@@ -43,7 +49,16 @@ public class TextBuddyTest {
 		testResult += "dcba\n";
 		assertEquals("sort fails", testResult, TextBuddy.executeCommand("sort"));
 	}
-	
+
+	/**
+	 * Sort on empty file
+	 */
+	@Test
+	public void testSort_3() {
+		assertEquals("sort fails", "text.txt is empty\n",
+				TextBuddy.executeCommand("sort"));
+	}
+
 	@Test
 	public void testSearch_1() {
 		TextBuddy.executeCommand("add one thing");
@@ -56,7 +71,53 @@ public class TextBuddyTest {
 				TextBuddy.executeCommand("search two"));
 	}
 
-	
-	
-	
+	// Not found
+	@Test
+	public void testSearch_2() {
+		TextBuddy.executeCommand("add one thing");
+		TextBuddy.executeCommand("add two things");
+		TextBuddy.executeCommand("add 1");
+		TextBuddy.executeCommand("add something else");
+
+		String testResult = "No line contains key word\n";
+		assertEquals("search fails", testResult,
+				TextBuddy.executeCommand("search three"));
+	}
+
+	@Test
+	public void testSearch_3() {
+		TextBuddy.executeCommand("add little brown fox");
+		TextBuddy.executeCommand("add jumped over the moon");
+		TextBuddy.executeCommand("add and jumped over the moon one more time");
+		TextBuddy.executeCommand("add but fell miserbly");
+
+		String testResult = "but fell miserbly\n";
+		assertEquals("search fails", testResult,
+				TextBuddy.executeCommand("search fell miserbly"));
+	}
+
+	/**
+	 * Search in an empty file
+	 */
+	@Test
+	public void testSearch_4() {
+		assertEquals("search fails", "text.txt is empty\n",
+				TextBuddy.executeCommand("search fell miserbly"));
+
+	}
+
+	/**
+	 * Search with empty keyword
+	 */
+	@Test
+	public void testSearch_5() {
+		TextBuddy.executeCommand("add little brown fox");
+		TextBuddy.executeCommand("add jumped over the moon");
+		TextBuddy.executeCommand("add and jumped over the moon one more time");
+		TextBuddy.executeCommand("add but fell miserbly");
+		assertEquals("search fails", "Empty keyword\n",
+				TextBuddy.executeCommand("search"));
+
+	}
+
 }
