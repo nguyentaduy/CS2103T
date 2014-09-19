3f48b2e5e9d2ba8d50499bcfe1acbd313df67465
Nguyen Ta Duy <nguyentaduy@hotmail.com>
2014-09-19 21:37:37 +0800

    unit test for sorting

--- src/TextBuddy/TextBuddyTest.java
+++ src/TextBuddy/TextBuddyTest.java
@@ -2,7 +2,6 @@ package TextBuddy;
 
 import static org.junit.Assert.*;
 
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,18 +16,32 @@ public class TextBuddyTest {
 	}
 	
 	@Test
-	public void testAdd() {
-		assertEquals("add fails", "added to text.txt: \"something\"\n", 
-				TextBuddy.executeCommand("add something"));
+	public void testSort_1() {
+
+		TextBuddy.executeCommand("add one thing");
+		TextBuddy.executeCommand("add two things");
+		TextBuddy.executeCommand("add 1");
+		TextBuddy.executeCommand("add something else");
+		String test = "";
+		test += "1\n";
+		test += "one thing\n";
+		test += "something else\n";
+		test += "two things\n";
+		assertEquals("sort fails", test, TextBuddy.executeCommand("sort"));
 	}
 
 	@Test
-	public void testDisplay() {
-		
-		TextBuddy.executeCommand("add one thing");
-		TextBuddy.executeCommand("add two things");
-		assertEquals("display fails", "1 one thing\n2 two things\n", 
-				TextBuddy.executeCommand("display"));
+	public void testSort_2() {
+		TextBuddy.executeCommand("add bcda");
+		TextBuddy.executeCommand("add cdba");
+		TextBuddy.executeCommand("add dcba");
+		TextBuddy.executeCommand("add acbd");
+		String testResult = "";
+		testResult += "acbd\n";
+		testResult += "bcda\n";
+		testResult += "cdba\n";
+		testResult += "dcba\n";
+		assertEquals("sort fails", testResult, TextBuddy.executeCommand("sort"));
 	}
 	
 	
